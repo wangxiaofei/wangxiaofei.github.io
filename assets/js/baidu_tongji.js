82010309cf3f8b0cda970c4c86806974abb4b785
wangxiaofei <xiaofei.wang@radida.com>
2015-01-09 22:13:05 +0800

    add Google Analytics

--- assets/js/baidu_tongji.js
+++
@@ -1,9 +0,0 @@
-// 百度统计
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?956e80262adaa163df11a076ab3ee5c1";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-
